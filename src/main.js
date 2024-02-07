d24146fb10a3fbe25ad4f00d0be4be0b6d087bc7
Juan de Dios Barajas <lextadz@gmail.com>
2024-02-07 16:46:25 -0600

    Bootrstrap added

--- src/main.js
+++ src/main.js
@@ -4,6 +4,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+/* Import Bootstrap */
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min'
+
 const app = createApp(App)
 
 app.use(router)
