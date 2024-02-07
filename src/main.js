465304eabdf1717f6f3d62669f0385bed6984db9
Juan de Dios Barajas <lextadz@gmail.com>
2024-02-07 16:51:31 -0600

    Font Awesome added

--- src/main.js
+++ src/main.js
@@ -8,8 +8,19 @@ import router from './router'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min'
 
+/* Import Font Awesome */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+library.add(fas, far, fab)
+
 const app = createApp(App)
 
 app.use(router)
 
 app.mount('#app')
+
+/*  Use Font Awesome */
+app.component('font-awesome-icon', FontAwesomeIcon)
